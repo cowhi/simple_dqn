1384bf2245a49df6dd7d7295fe9e727c0dca9882
Ruben Glatt <ruben.glatt@gmail.com>
2016-05-28 14:41:33 -0300

    minor update

--- src/agent.py
+++ src/agent.py
@@ -100,7 +100,7 @@ class Agent:
       if self.mem.count > self.mem.batch_size and i % self.train_frequency == 0:
         # train for train_repeat times
         for j in xrange(self.train_repeat):
-          logger.info("i=%d, j=%d, mem.count=%d" % (i, j, self.mem.count))
+          #logger.info("i=%d, j=%d, mem.count=%d" % (i, j, self.mem.count))
           # sample minibatch
           minibatch = self.mem.getMinibatch()
           # train the network
